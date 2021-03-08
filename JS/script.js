430d4ee2a5fe945d5f5d2c2760b993e186323b26
pricilagarcia <68708787+pricilagarcia@users.noreply.github.com>
2021-03-08 09:56:43 -0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,26 @@
+//VARIABLES Y TIPOS DE DATOS EN JAVASCRI
+var saludo = "Hola JavaScrip"
+var edad = 6
+var siNo = true
+var seccion = 'a'
+
+
+//MOSTRAR UN DATO EN CONSOLA
+console.log (saludo)
+
+//CONDICIONALES
+
+edad =prompt("Edad")
+
+if(edad >=18){
+    alert("eres mayor de edad")
+    
+    var usuario = prompt("usuario: ")
+    
+}else{
+    alert("eres menor de edad")
+}
+   
+
+
+
